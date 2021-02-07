36f9ea38863f1430b94e830e61249fde1b1f07bb
agc <gaffar.csejust@gmail.com>
2021-02-07 14:50:14 +0600

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,268 @@
+
+        function bootstrap() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#bootstrap").css({
+                    "width": y
+                });
+
+                $("#bootstrap").html(y);
+                counter++;
+                if (counter === 76) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function html() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#html").css({
+                    "width": y
+                });
+
+                $("#html").html(y);
+                counter++;
+                if (counter === 66) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function JQUERY() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#JQUERY").css({
+                    "width": y
+                });
+
+                $("#JQUERY").html(y);
+                counter++;
+                if (counter === 71) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function VUE() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#VUE").css({
+                    "width": y
+                });
+
+                $("#VUE").html(y);
+                counter++;
+                if (counter === 36) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function AJAX() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#AJAX").css({
+                    "width": y
+                });
+
+                $("#AJAX").html(y);
+                counter++;
+                if (counter === 26) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+        $("#Technical").click(function() {
+            bootstrap();
+            html();
+            JQUERY();
+            VUE();
+            AJAX();
+        });
+
+   
+
+
+ 
+        function PHP() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#PHP").css({
+                    "width": y
+                });
+
+                $("#PHP").html(y);
+                counter++;
+                if (counter === 76) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Laravel() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Laravel").css({
+                    "width": y
+                });
+
+                $("#Laravel").html(y);
+                counter++;
+                if (counter === 66) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Mysql() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Mysql").css({
+                    "width": y
+                });
+
+                $("#Mysql").html(y);
+                counter++;
+                if (counter === 71) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Wordpress() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Wordpress").css({
+                    "width": y
+                });
+
+                $("#Wordpress").html(y);
+                counter++;
+                if (counter === 36) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        $("#Technical").click(function() {
+            PHP();
+            Laravel();
+            Mysql();
+            Wordpress();
+        });
+
+    
+   
+        function C() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#C").css({
+                    "width": y
+                });
+
+                $("#C").html(y);
+                counter++;
+                if (counter === 76) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Cpp() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Cpp").css({
+                    "width": y
+                });
+
+                $("#Cpp").html(y);
+                counter++;
+                if (counter === 66) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Java() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Java").css({
+                    "width": y
+                });
+
+                $("#Java").html(y);
+                counter++;
+                if (counter === 71) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Android() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Android").css({
+                    "width": y
+                });
+
+                $("#Android").html(y);
+                counter++;
+                if (counter === 36) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        function Valina() {
+            var counter = 0;
+            var i = setInterval(function() {
+                let y = counter + "%";
+                $("#Valina").css({
+                    "width": y
+                });
+
+                $("#Valina").html(y);
+                counter++;
+                if (counter === 36) {
+                    clearInterval(i);
+                }
+            }, 150);
+
+        }
+
+        $("#Technical").click(function() {
+            C();
+            Cpp();
+            Java();
+            Android();
+            Valina();
+        });
+
+   
